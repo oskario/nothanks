5ae4266d2d38588b966fb98ba85cd7ea9eb26967
Oskar <oskario2007@gmail.com>
2014-03-15 21:36:45 +0100

    Logging service added

---
+++ app/services/LoggingService.scala
@@ -0,0 +1,73 @@
+package models
+
+import akka.actor.Actor
+import akka.actor.Props
+import akka.actor.actorRef2Scala
+import play.api.Logger
+import play.api.Play.current
+import play.api.libs.concurrent.Akka
+
+/**
+ * Logging service.
+ *
+ * Offers dynamic, extensive and configurable logging system.
+ */
+object LoggingService {
+  import models.LoggingServiceProtocol._
+
+  private val actor = Akka.system.actorOf(Props[LoggingService])
+
+  def error(message: String) = {
+    actor ! LogError(message)
+  }
+
+  def error(sender: String, message: String) = {
+    actor ! LogError(s"[$sender] $message")
+  }
+
+  def info(message: String) = {
+    actor ! LogInfo(message)
+  }
+
+  def info(sender: String, message: String) = {
+    actor ! LogInfo(s"[$sender]: $message")
+  }
+
+  def debug(message: String) = {
+    actor ! LogDebug(message)
+  }
+}
+
+/**
+ * Protocol used by the logging service.
+ */
+object LoggingServiceProtocol {
+  case class LogError(message: String)
+  case class LogInfo(message: String)
+  case class LogDebug(message: String)
+}
+
+/**
+ * Logging service actor.
+ *
+ * Defines custom logging procedures (e.g. broadcasting messages to other log provider, saving to file, etc.).
+ */
+class LoggingService extends Actor {
+  import models.LoggingServiceProtocol._
+
+  val logger = Logger
+
+  def receive = {
+    case LogError(msg) => {
+      logger.error(msg)
+    }
+
+    case LogInfo(msg) => {
+      logger.info(msg)
+    }
+
+    case LogDebug(msg) => {
+      logger.debug(msg)
+    }
+  }
+}
